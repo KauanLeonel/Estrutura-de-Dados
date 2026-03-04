07ba23e2fd7357756b1863ba00cfafbb5c43bd65
Kauan Leonel <kauan.br.char@gmail.com>
2026-03-04 20:01:10 -0300

    Ordenação: - Maior Menor - Insertition - Melhorado

--- Aula03/Inverso.java
+++ Aula03/Inverso.java
@@ -4,6 +4,7 @@ public class Inverso {
 
     public static void main(String[] args) {
 
+        //#region VARIÁVEIS
         int N = 10000;
 
         int vetAleatorio_1[] = new int[N];
@@ -18,8 +19,20 @@ public class Inverso {
         int vetMelhorCaso_2[] = vetMelhorCaso_1.clone();
         int vetPiorCaso_2[] = vetPiorCaso_1.clone();
 
-        long inicio, fim, tempoS, tempoMS;
+        int vetAleatorio_3[] = vetAleatorio_1.clone();
+        int vetMelhorCaso_3[] = vetMelhorCaso_1.clone();
+        int vetPiorCaso_3[] = vetPiorCaso_1.clone();
 
+        
+        int vetAleatorio_4[] = vetAleatorio_1.clone();
+        int vetMelhorCaso_4[] = vetMelhorCaso_1.clone();
+        int vetPiorCaso_4[] = vetPiorCaso_1.clone();
+
+
+        long inicio, fim, tempoS, tempoMS;
+        //#endregion
+
+        //#region ORDENAÇÃO RUIM
         //////// ORDENAÇÃO RUIM ! /////////////////
         
         //--- ALEATORIO ---//
@@ -46,7 +59,8 @@ public class Inverso {
         tempoMS = (fim - inicio) - tempoS * 1000;
         System.out.println("Tempo de ordenacao Pior Caso: " + tempoS + "s" + tempoMS + "ms");
 
-        //////// ORDENAÇÃO MELHORADO ! /////////////////
+        //#endregion
+        //#region ORDENAÇÃO MELHORADO ! 
         
         //--- ALEATORIO ---//
         inicio = System.currentTimeMillis();
@@ -72,6 +86,65 @@ public class Inverso {
         tempoMS = (fim - inicio) - tempoS * 1000;
         System.out.println("Tempo de ordenacao(melhorado) Pior Caso: " + tempoS + "s" + tempoMS + "ms");
         // exibirVetor(vetPiorCaso_2, N);
+
+        //#endregion
+
+           //#region ORDENAÇÃO insertion ! 
+        
+        //--- ALEATORIO ---//
+        inicio = System.currentTimeMillis();
+        insertion(vetAleatorio_3, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(insertion) Aleatorio: " + tempoS + "s" + tempoMS + "ms");
+
+        //--- MELHOR CASO ---//
+        inicio = System.currentTimeMillis();
+        insertion(vetMelhorCaso_3, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(insertion) Melhor Caso: " + tempoS + "s" + tempoMS + "ms");
+
+        //--- PIOR CASO ---//
+        inicio = System.currentTimeMillis();
+        insertion(vetPiorCaso_3, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(insertion) Pior Caso: " + tempoS + "s" + tempoMS + "ms");
+        // exibirVetor(vetPiorCaso_2, N);
+        //#endregion
+
+        
+        //#region ORDENAÇÃO MAIOR MENOR ! 
+        
+        //--- ALEATORIO ---//
+        inicio = System.currentTimeMillis();
+        ordenarMaiorMenor(vetAleatorio_4, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(ordenarMaiorMenor) Aleatorio: " + tempoS + "s" + tempoMS + "ms");
+
+        //--- MELHOR CASO ---//
+        inicio = System.currentTimeMillis();
+        ordenarMaiorMenor(vetMelhorCaso_4, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(ordenarMaiorMenor) Melhor Caso: " + tempoS + "s" + tempoMS + "ms");
+
+        //--- PIOR CASO ---//
+        inicio = System.currentTimeMillis();
+        ordenarMaiorMenor(vetPiorCaso_4, N);
+        fim = System.currentTimeMillis();
+        tempoS = (fim - inicio) / 1000;
+        tempoMS = (fim - inicio) - tempoS * 1000;
+        System.out.println("Tempo de ordenacao(ordenarMaiorMenor) Pior Caso: " + tempoS + "s" + tempoMS + "ms");
+        // exibirVetor(vetPiorCaso_2, N);
+        //#endregion
     }
 
     static void inicializarVetor(int vet[], int N, int s) {
@@ -121,6 +194,48 @@ public class Inverso {
         }
     }
 
+        static void insertion(int[] vet, int N){
+        
+        for (int i = 1; i < N; ++i) {
+            int key = vet[i];
+            int j = i - 1;
+
+            while (j >= 0 && vet[j] > key) {
+                vet[j + 1] = vet[j];
+                j = j - 1;
+            }
+            vet[j + 1] = key;
+        }
+    }
+
+     private static void ordenarMaiorMenor(int[] vet, int N) {
+
+        int contMenor = 0;
+        int contMaior = N - 1;
+        int menor = vet[0];
+        int maior = vet[0];
+        int posMenor = 0;
+        int posMaior = 0;
+
+        while (contMenor < contMaior) {
+            for (int j = contMenor + 1; j < contMaior; j++) {
+               if(vet[j] < menor){
+                   menor = vet[j];
+                   posMenor = j;
+               }
+               if(vet[j] > maior){
+                   maior = vet[j];
+                   posMaior = j;
+               }
+               
+            }
+            troca(vet, contMenor, posMenor);
+            troca(vet, contMaior, posMaior);
+            
+            contMenor++;
+            contMaior--;
+        }
+    }
     private static void ordenarMelhorado(int[] vet, int N) {
         for (int i = 0; i < N; i++) {
             boolean comp = true;     
@@ -128,16 +243,11 @@ public class Inverso {
                         if (vet[j] > vet[j + 1]) {
                             comp = false;
                             troca(vet, j, j + 1);
-                        }
-                        if(vet[vet.length - 1 - j] > vet[vet.length - j - 2] ){
-                            troca(vet, vet.length - 1 - j, vet.length - j - 2);
-                        }
-
                     }
                   if (comp) break;
                 }
             }
         }
-
+    }
     
     
